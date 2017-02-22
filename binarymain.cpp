472c6e1b5831171f5b810c55aefe8783665772a2
Kai <kwok2549kai@gmail.com>
2017-02-22 15:21:55 -0800

    Added binarymain.cpp which implements the traditional hello world exercise in binary language.  Fixes #1 and #2 in issues

---
+++ binarymain.cpp
@@ -0,0 +1,10 @@
+//binarymain.cpp
+#include <iostream>
+
+int main()
+{
+	//"Hello World!" translated by www.binarytranslator.com
+	std::cout<<"01001000 01100101 01101100 01101100 01101111 00100000 01010111 01101111 01110010 01101100 01100100 00100001"<<std::endl;
+
+	return 0;
+}
